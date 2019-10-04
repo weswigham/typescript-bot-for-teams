a2f86f0cdac67f6c272915309a1a2bead6a3ae03
Wesley Wigham <wewigham@microsoft.com>
2019-10-04 15:49:43 -0700

    Initial scheduler bot with deployment script

---
+++ src/bot.ts
@@ -0,0 +1,84 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { TurnContext, BotState, ConversationReference, Storage } from 'botbuilder';
+import { TeamsAdapter } from 'botbuilder-teams';
+import { schedule, validate, ScheduledTask } from "node-cron";
+
+const scheduleMessageRegex = /schedule standup here on (.*)/;
+
+interface BotStorageSchema {
+    cronExpr: { value: string, eTag: string; };
+    ref: { value: Partial<ConversationReference>, eTag: string; };
+}
+
+export class TypescriptStandupBot {
+    private task: ScheduledTask | undefined;
+    constructor(
+        private conversationState: BotState,
+        private storage: Storage,
+        private adapter: TeamsAdapter
+    ) {
+        // This is potentially racy if a message arives and is handled before this is done
+        // If that turns out to be problematic, we can try to queue turns while initialization
+        // is ongoing - but that's quite a bit for an edge case that might not be a problem.
+        this.setupAsync();
+    }
+
+    async setupAsync() {
+        const data: Partial<BotStorageSchema> = await this.storage.read(["ref", "cronExpr"]);
+        if (data.cronExpr && data.ref) {
+            await this.setupStandupCron(data.cronExpr.value, data.ref.value, /*save*/ false);
+        }
+    }
+
+    async postStandupThread(ref: Partial<ConversationReference>) {
+        this.adapter.continueConversation(ref, async context => {
+            const date = new Date();
+            await this.adapter.createReplyChain(context, [{
+                // Month is zero indexed, and day is just off by one?!?
+                text: `*Standup ${date.getFullYear()}-${date.getMonth() + 1}-${date.getDay() - 1}*` 
+            }]);
+        });
+    }
+
+    async setupStandupCron(cronExpr: string, ref: Partial<ConversationReference>, save: boolean) {
+        if (this.task) {
+            this.task.stop().destroy();
+        }
+        this.task = schedule(cronExpr, () => {
+            this.postStandupThread(ref);
+        });
+        if (save) {
+            const toStore: BotStorageSchema = {
+                cronExpr: { value: cronExpr, eTag: "*" },
+                ref: { value: ref, eTag: "*" }
+            };
+            await this.storage.write(toStore);
+        }
+    }
+
+    async handleSceduleStandupMessage(turnContext: TurnContext, cronExpr: string) {
+        if (!validate(cronExpr)) {
+            await turnContext.sendActivity(`'${cronExpr}' is not a valid cron expression. Try something like '30 10 * * Monday,Tuesday,Wednesday,Thursday,Friday', which would schedule a standup at 10:30 each weekday.`);
+            return;
+        }
+        const ref = TurnContext.getConversationReference(turnContext.activity);
+        if (this.task) {
+            await turnContext.sendActivity("Existing standup schedule canceled, resceduling...");
+        }
+        await this.setupStandupCron(cronExpr, ref, /*save*/ true);
+        await turnContext.sendActivity(`Standups scheduled for '${cronExpr}'.`);
+    }
+
+    async onTurn(turnContext: TurnContext) {
+        if (turnContext.activity.type === "message") {
+            const text = turnContext.activity.text;
+            const match = scheduleMessageRegex.exec(text);
+            if (match) {
+                await this.handleSceduleStandupMessage(turnContext, match[1]);
+            }
+        }
+        await this.conversationState.saveChanges(turnContext);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { config } from 'dotenv';
+import * as path from 'path';
+import * as restify from 'restify';
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+import { MemoryStorage } from 'botbuilder';
+
+// This bot's main dialog.
+import { TypescriptStandupBot } from './bot';
+import { TeamsMiddleware, TeamSpecificConversationState, TeamsAdapter } from 'botbuilder-teams';
+import { BlobStorage } from 'botbuilder-azure';
+
+const ENV_FILE = path.join(__dirname, '..', '.env');
+config({ path: ENV_FILE });
+
+// Create HTTP server.
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+    console.log(`\nTo test your bot, see: https://aka.ms/debug-with-emulator`);
+});
+
+const storage = process.env.StorageAccount && process.env.StorageAccessKey && process.env.StorageContainerName ? new BlobStorage({
+    containerName: process.env.StorageContainerName,
+    storageAccountOrConnectionString: process.env.StorageAccount,
+    storageAccessKey: process.env.storageAccessKey
+}) : new MemoryStorage();
+const botState = new TeamSpecificConversationState(storage);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new TeamsAdapter({
+    appId: process.env.MicrosoftAppID,
+    appPassword: process.env.MicrosoftAppPassword,
+});
+adapter.use(new TeamsMiddleware());
+
+// Catch-all for any unhandled errors in your bot.
+adapter.onTurnError = async (context, error) => {
+    // This check writes out errors to console log .vs. app insights.
+    console.error(`\n [onTurnError]: ${ error }`);
+    // Send a message to the user.
+    await context.sendActivity(`Oops. Something went wrong!`);
+    // Clear out state
+    await botState.delete(context);
+};
+
+const bot = new TypescriptStandupBot(botState, storage, adapter);
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    // Use the adapter to process the incoming web request into a TurnContext object.
+    adapter.processActivity(req, res, bot.onTurn.bind(bot));
+});
